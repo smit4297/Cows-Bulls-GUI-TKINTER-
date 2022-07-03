687913a2611fb840c43ceaaa71e7c0a6cd629a99
smit patel <55511824+smit4297@users.noreply.github.com>
2022-07-04 01:58:21 +0530

    Add files via upload
    Updated code such that it works on any number of digits.

--- MainGame.py
+++ MainGame.py
@@ -11,38 +11,41 @@ class GameState():
     '''
 
     def __init__(self):
-        self.round = 1
-        self.secret_number = self.generate_random()
+        self.score = 1
+        self.number_len = None
+        self.secret_number = None
         self.previous_guesses = []
-        self.running = True
-        self.new_round()
 
-    def new_round(self):
+    def new_score(self):
         '''
-        start new round
+        start new score
         '''
-        self.round = 1
+        self.score = 1
+        self.number_len = None
         self.update_secret_number()
 
     def generate_random(self):
+        range_start = 10 ** (int(self.number_len) - 1)
+        range_end = (10 ** int(self.number_len)) - 1
         while True:
-            number = random.randint(1000, 9999)
+            number = random.randint(range_start, range_end)
             if len(str(number)) == len(set(list(str(number)))):
                 return str(number)
 
     def update_secret_number(self):
         '''
-        generates new secret number at the beginning of new round
+        generates new secret number at the beginning of new game
         '''
-        self.secret_number = self.generate_random()
+        self.secret_number = None
 
 
 def start_new_game():
     '''
     Checks the current state and starts new game
     '''
+
     GUI.reset_gui()
-    STATE.new_round()
+    STATE.new_score()
 
 
 def submit_guess(game_state):
@@ -51,6 +54,12 @@ def submit_guess(game_state):
     '''
     guessed_number = GUI.player_tip.get()
     print(game_state.secret_number)
+    if game_state.number_len == None:
+        GUI.game_result.config(text="Please Enter Number Of Digits Before Guessing...")
+        return
+    if guessed_number == "":
+        GUI.game_result.config(text='Please Guess The Number To Proceed')
+        return
     if len(guessed_number) != len(game_state.secret_number):
         ROOT.bell()
         GUI.game_result.config(text=f'Number length must be {len(game_state.secret_number)}')
@@ -59,15 +68,31 @@ def submit_guess(game_state):
         ROOT.bell()
         GUI.game_result.config(text=f'All digits must be unique')
         return
-    game_state.round += 1
-    GUI.update_round(game_state.round)
+    game_state.score += 1
+    GUI.update_score(game_state.score)
     game_state.previous_guesses.append(guessed_number)
     cows = Cow(game_state.secret_number, guessed_number)
     bulls = Bull(game_state.secret_number, guessed_number)
     if str(cows) == str(len(game_state.secret_number)):
-        game_state.running = False
-        GUI.game_result.config(text=f"Finally, You Did IT\nYour previous guesses are : {game_state.previous_guesses[0:-1]}")
+        GUI.game_result.config(
+            text=f"Finally, You Did IT\nYour previous guesses are : {game_state.previous_guesses[0:-1]}")
     GUI.update_bulls_cows([str(cows), str(bulls)])
+
+
+def generate_secret_code(game_state):
+    digit_length = GUI.digits.get()
+    #if will raise an exception if i click on okay without writing anything
+    try:
+        if int(digit_length) >= 1 and int(digit_length) <= 10:
+            GUI.number_of_digits = int(digit_length)
+            game_state.number_len = digit_length
+            game_state.secret_number = game_state.generate_random()
+            print(game_state.secret_number)
+            GUI.game_result.config(text=f"Secret Number of {digit_length} digit Has Been Generated")
+        else:
+            GUI.game_result.config(text="0 < Number of digits < 11")
+    except:
+        GUI.game_result.config(text="Please Enter The Number Of Digits To Proceed")
 
 
 if __name__ == '__main__':
@@ -81,7 +106,7 @@ if __name__ == '__main__':
     GUI.new_game_button.bind('<Button-1>',
                              lambda event:
                              start_new_game())
-    GUI.player_tip.bind('<Return>',
-                        lambda event, game_state=STATE:
-                        submit_guess(STATE))
+    GUI.okay_button.bind('<Button-1>',
+                         lambda event, game_state=STATE:
+                         generate_secret_code(STATE))
     ROOT.mainloop()
